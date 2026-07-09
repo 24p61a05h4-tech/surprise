53a0f404c5d791015c37f897d9589092304f910c
24p61a05h4-tech <24p61a05h4@vbithyd.ac.in>
2026-07-09 19:11:55 +0530

    Add files via upload

---
+++ data/memories.js
@@ -0,0 +1,51 @@
+const memories = [
+{
+    type:"image",
+    file:"photo1.jpeg",
+    date:"12 Jan 2026",
+    caption:"First memory ❤️"
+},
+{
+    type:"image",
+    file:"photo2.jpeg",
+    date:"16 Feb 2026",
+    caption:"Movie day 🎬"
+
+},
+{
+    type:"image",
+    file:"photo3.jpeg",
+    date:"16 Feb 2026",
+    caption:"Movie day 🎬"
+},
+{
+    type:"image",
+    file:"photo4.jpeg",
+    date:"16 Feb 2026",
+    caption:"Movie day 🎬"
+},
+{
+    type:"image",
+    file:"photo5.jpeg",
+    date:"16 Feb 2026",
+    caption:"Movie day 🎬"
+},
+{
+    type:"video",
+    file:"photo6.mp4",
+    date:"10 Jun 2026",
+    caption:"Best video ever 😂"
+},
+{
+    type:"video",
+    file:"photo7.mp4",
+    date:"21 Apr 2026",
+    caption:"Couldn't stop laughing 😂"
+},
+{
+    type:"image",
+    file:"photo8.jpeg",
+    date:"16 Feb 2026",
+    caption:"Movie day 🎬"
+},
+];
